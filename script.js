294fb0814f3449a651c6a83ede43fbb3478ab105
uaeboss <133223488+uaeboss@users.noreply.github.com>
2023-06-02 15:50:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+    var x = document.getElementById("navRight");
+    if (x.className === "nav-right") {
+      x.className += "responsive";
+    } else {
+      x.className = "nav-right";
+    }
+  }
